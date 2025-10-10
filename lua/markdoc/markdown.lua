ff4bc62f22250c51b0be5bec4a1e55c3c2a1b02b
Shawon <mdmoinulhossainshawon@gmail.com>
2025-10-10 06:14:33 +0600

    feat(markdown): Initial structure of the tree walker

--- lua/markdoc/markdown.lua
+++ lua/markdoc/markdown.lua
@@ -1,60 +1,88 @@
 local markdown = {};
+local config = require("markdoc.config");
 
-markdown.wrap_buf = vim.api.nvim_create_buf(false, true);
+---@param TSNode? TSNode
+local function clear_node (buffer, TSNode)
+	---|fS
 
-markdown.wrap = function (lines, width, comment)
-	if not markdown.wrap_buf or not vim.api.nvim_buf_is_valid(markdown.wrap_buf) then
-		markdown.wrap_buf = vim.api.nvim_create_buf(false, true);
+	if not TSNode then
+		return;
 	end
 
-	vim.api.nvim_buf_set_lines(markdown.wrap_buf, 0, -1, false, lines);
+	local R = { TSNode:range() };
 
-	vim.bo[markdown.wrap_buf].filetype = "vimdoc";
-	vim.bo[markdown.wrap_buf].textwidth = width;
-	vim.bo[markdown.wrap_buf].comments = comment;
+	vim.api.nvim_buf_set_text(buffer, R[1], R[2], R[3], R[4], {});
 
-	vim.api.nvim_buf_call(markdown.wrap_buf, function ()
-		vim.cmd("normal! gggqG");
-	end);
-
-	return vim.api.nvim_buf_get_lines(markdown.wrap_buf, 0, -1, false);
-end
-
-markdown.to_block = function (buffer, TSNode)
-	local range = { TSNode:range() };
-
-	if range[2] == 0 then
-		return vim.split(
-			vim.treesitter.get_node_text(TSNode, buffer, {}),
-			"\n"
-		);
-	end
-
-	local lines = vim.api.nvim_buf_get_lines(buffer, range[1], range[3], false);
-
-	for l, line in ipairs(lines) do
-		lines[l] = string.sub(line, range[2]);
-	end
-
-	return lines;
+	---|fE
 end
 
 ---@param buffer integer
 ---@param TSNode TSNode
-markdown.paragraph = function (buffer, _, TSNode)
-	local wrapped = markdown.wrap(
-		markdown.to_block(buffer, TSNode),
-		20
-	);
+markdown.atx_heading = function (buffer, _, TSNode)
+	local _marker = TSNode:named_child(0) --[[ @as TSNode ]];
+	local marker = vim.treesitter.get_node_text(_marker, buffer, {});
+	local heading = {};
+
+	local MAX = config.active.textwidth or (vim.bo[buffer].textwidth > 0 and vim.bo[buffer].textwidth or 80);
+
+	local _content = TSNode:field("heading_content")[1];
+
+	local function tag_lines ()
+		local text = vim.treesitter.get_node_text(_content, buffer, {});
+		local tags = config.get_tags(text);
+
+		local _tmp = text;
+		local _tags = "";
+
+		for _, tag in ipairs(tags) do
+			local W = vim.fn.strdisplaywidth(_tmp .. _tags);
+
+			if vim.fn.strdisplaywidth(tag) + 2 >= MAX then
+				table.insert(heading, _tmp);
+				table.insert(heading, string.format("*%s*", tag));
+
+				_tmp = "";
+			elseif (vim.fn.strdisplaywidth(tag) + 3) + W <= MAX then
+				_tags = _tags .. string.format(" *%s*", tag);
+			else
+				table.insert(heading, _tmp .. string.rep(" ", MAX - W) .. _tags)
+				_tmp = "";
+				_tags = "";
+			end
+		end
+
+		if _tags ~= "" then
+			table.insert(heading, _tmp .. string.rep(" ", MAX - vim.fn.strdisplaywidth(_tmp .. _tags)) .. _tags)
+		end
+	end
+
+	if not _content then
+		goto no_content;
+	end
+
+	if #marker == 1 then
+		table.insert(heading, string.rep("=", MAX));
+		tag_lines();
+	elseif #marker == 2 then
+		table.insert(heading, string.rep("-", MAX));
+		tag_lines();
+	elseif #marker == 3 then
+		tag_lines();
+	else
+	end
+
+	::no_content::
 
 	local R = { TSNode:range() };
-
-	vim.api.nvim_buf_set_text(buffer, R[1], R[2], R[3], R[4], wrapped);
+	vim.api.nvim_buf_set_text(buffer, R[1], R[2], R[3] - 1, -1, heading);
 end
 
-markdown.rule_map = {
-	{ "(paragraph) @paragraph", markdown.paragraph },
+
+markdown.pre_rule = {
+	{ "(atx_heading) @atx", markdown.atx_heading }
 };
+markdown.post_rule = {};
+
 
 markdown.transform = function (TSTree, buffer, rule)
 	local query = vim.treesitter.query.parse("markdown", rule[1]);
@@ -66,12 +94,14 @@ markdown.transform = function (TSTree, buffer, rule)
 	end
 
 	for _, item in ipairs(stack) do
-		pcall(rule[2], buffer, item[1], item[2]);
+		vim.print(
+			pcall(rule[2], buffer, item[1], item[2])
+		)
 	end
 end
 
 markdown.walk = function (buffer)
-	for _, rule in ipairs(markdown.rule_map) do
+	for _, rule in ipairs(markdown.pre_rule) do
 		local root_parser = vim.treesitter.get_parser(buffer);
 
 		if not root_parser then
@@ -89,12 +119,30 @@ markdown.walk = function (buffer)
 		end);
 	end
 
-	vim.print(
-		table.concat(
-			vim.api.nvim_buf_get_lines(buffer, 0, -1, false),
-			"\n"
-		)
-	)
+	vim.api.nvim_buf_call(buffer, function ()
+		vim.bo[buffer].textwidth = config.active.textwidth;
+		vim.cmd("normal! gggqG");
+	end)
+
+	for _, rule in ipairs(markdown.post_rule) do
+		local root_parser = vim.treesitter.get_parser(buffer);
+
+		if not root_parser then
+			return;
+		end
+
+		root_parser:parse(true);
+
+		root_parser:for_each_tree(function (TSTree, language_tree)
+			local lang = language_tree:lang();
+
+			if lang == "markdown" then
+				markdown.transform(TSTree, buffer, rule)
+			end
+		end);
+	end
+
+	vim.api.nvim_open_win(buffer, false, { split = "right" });
 end
 
 return markdown;
