ad1a206ffea0b0518a59507c4b6de5bf4735f768
Shawon <mdmoinulhossainshawon@gmail.com>
2025-10-10 06:13:48 +0600

    feat: Added config

---
+++ lua/markdoc/config.lua
@@ -0,0 +1,52 @@
+local config = {};
+
+config.eval = function (val, ...)
+	if type(val) ~= "function" then
+		return val;
+	end
+
+	local sucess, new = pcall(val, ...);
+
+	if sucess then
+		return new;
+	end
+end
+
+config.default = {
+	textwidth = 80,
+	indent = 4,
+
+	tags = {
+		default = { "a", "b", "c" }
+	},
+	block_quotes = {},
+	list_items = {}
+};
+
+config.active = vim.deepcopy(config.default, true);
+
+config.get_tags = function (text)
+	if not config.active.tags then
+		return {};
+	end
+
+	local keys = vim.tbl_keys(config.active.tags or {});
+
+	for _, key in ipairs(keys) do
+		if string.match(text, key) then
+			return config.eval(config.active.tags[key], text);
+		end
+	end
+
+	return config.eval(config.active.tags.default, text);
+end
+
+config.setup = function (new)
+	if type(new) ~= "table" then
+		return;
+	end
+
+	config.active = vim.tbl_deep_extend("force", config.active, new);
+end
+
+return config;
